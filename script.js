2b706b6307d870335d1b93a283d22498d45fec65
shivagnanadeep <147229586+shivagnanadeep@users.noreply.github.com>
2024-05-18 22:47:24 +0530

    creating variables for every modifiable object in the HTML Document

---
+++ script.js
@@ -0,0 +1,5 @@
+let offSwitch=document.getElementById("offSwitch");
+let onSwitch = document.getElementById("onSwitch");
+let bulbImage=document.getElementById("bulbImage");
+let catImage=document.getElementById("catImage");
+let switchStatus=document.getElementById("switchStatus");
